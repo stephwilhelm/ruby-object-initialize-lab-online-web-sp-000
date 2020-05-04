ca8b6a7b5fe7a160d7901cbb04a9d656a50feb7c
Steph W <wilhelm.steph@gmail.com>
2020-05-04 00:19:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
+    @breed = breed
   end
 
   def name=(name)
@@ -9,10 +10,6 @@ class Dog
 
   def name
     @name
-  end
-
-  def initialize(breed)
-    @breed = breed
   end
 
   def breed=(breed)
